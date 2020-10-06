6933b9839c7d850318e22d7a39a1d721bc299790
Kevin Cuadros <kev040798@gmail.com>
2020-10-05 22:31:49 -0400

    Finish story 4

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,5 +1,21 @@
 class SheltersController < ApplicationController
   def index
-    @shelters = ['Shelter 1', 'Shelter 2', 'Shelter 3']
+    @shelters = Shelter.all
+  end
+
+  def new; end
+
+  def create
+    shelter = Shelter.new({
+                            name: params[:shelter][:name],
+                            address: params[:shelter][:address],
+                            city: params[:shelter][:city],
+                            state: params[:shelter][:state],
+                            zip: params[:shelter][:zip]
+                          })
+
+    shelter.save
+
+    redirect_to '/shelters'
   end
 end
