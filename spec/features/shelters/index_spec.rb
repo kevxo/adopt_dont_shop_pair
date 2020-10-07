09a66c65d9a78b9e5c88571ba277983e9205f19e
Kevin Cuadros <kev040798@gmail.com>
2020-10-07 19:29:11 -0400

    Test for story 3 and update index test

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -3,29 +3,29 @@ require 'rails_helper'
 describe 'As a visitor' do
   describe "When I visit '/shelters' " do
     it 'Then I see the name of each shelter in the system' do
-      shelter_1 = Shelter.create(name: 'Dogs and Cats',
-                                 address: '1234 spoon.st',
-                                 city: 'Tampa',
-                                 state: 'Florida',
-                                 zip: '34638')
-      shelter_2 = Shelter.create(name: 'Pets are Us',
-                                  address: '1894 Lincoln.st',
-                                  city: 'Tampa',
-                                  state: 'Florida',
-                                  zip: '32938')
+      shelter1 = Shelter.create(name: 'Dogs and Cats',
+                                address: '1234 spoon.st',
+                                city: 'Tampa',
+                                state: 'Florida',
+                                zip: '34638')
+      shelter2 = Shelter.create(name: 'Pets are Us',
+                                address: '1894 Lincoln.st',
+                                city: 'Tampa',
+                                state: 'Florida',
+                                zip: '32938')
       visit '/shelters'
 
-      expect(page).to have_content("All Shelters")
-      expect(page).to have_content("#{shelter_1.name}")
-      expect(page).to have_content("#{shelter_2.name}")
-      expect(page).to have_content("#{shelter_1.address}")
-      expect(page).to have_content("#{shelter_2.address}")
-      expect(page).to have_content("#{shelter_1.city}")
-      expect(page).to have_content("#{shelter_2.city}")
-      expect(page).to have_content("#{shelter_1.state}")
-      expect(page).to have_content("#{shelter_2.state}")
-      expect(page).to have_content("#{shelter_1.zip}")
-      expect(page).to have_content("#{shelter_2.zip}")
+      expect(page).to have_content('All Shelters')
+      expect(page).to have_content(shelter1.name.to_s)
+      expect(page).to have_content(shelter2.name.to_s)
+      expect(page).to have_content(shelter1.address.to_s)
+      expect(page).to have_content(shelter2.address.to_s)
+      expect(page).to have_content(shelter1.city.to_s)
+      expect(page).to have_content(shelter2.city.to_s)
+      expect(page).to have_content(shelter1.state.to_s)
+      expect(page).to have_content(shelter2.state.to_s)
+      expect(page).to have_content(shelter1.zip.to_s)
+      expect(page).to have_content(shelter2.zip.to_s)
     end
   end
 end
---
+++ spec/features/shelters/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe "When I visit '/shelters/:id' " do
+    it "should display the shelter's id and information" do
+      shelter1 = Shelter.create(name: 'Dogs and Cats',
+                                 address: '1234 spoon.st',
+                                 city: 'Tampa',
+                                 state: 'Florida',
+                                 zip: '34638')
+
+      visit "/shelters/#{shelter_1.id}"
+
+      expect(page).to have_content(shelter1.id.to_s)
+      expect(page).to have_content(shelter1.name.to_s)
+      expect(page).to have_content(shelter1.address.to_s)
+      expect(page).to have_content(shelter1.city.to_s)
+      expect(page).to have_content(shelter1.state.to_s)
+      expect(page).to have_content(shelter1.zip.to_s)
+    end
+  end
+end
