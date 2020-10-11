f6040ed2470fa0a2ca9fc447a53500994d95f6b1
Kevin Cuadros <kev040798@gmail.com>
2020-10-11 18:39:30 -0400

    Finish the model test

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 describe 'As a visitor' do
   describe "when I visit '/pets/:id'" do
     it 'should see the pet with that id including pets information' do
-      pet1 = Pet.create(img: "https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg",
+      shelter1 = Shelter.create(name: 'Dogs and Cats',
+                                address: '1234 spoon.st',
+                                city: 'Tampa',
+                                state: 'Florida',
+                                zip: '34638')
+      pet1 = Pet.create(img: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg',
                         name: 'Mittens',
                         description: "He's healthy",
                         approximate_age: '6 years',
                         sex: 'Male',
-                        adoptable: 'Yes')
+                        adoptable: 'Yes',
+                        shelter_id: shelter1.id)
 
       visit "pets/#{pet1.id}"
       expect(page).to have_content(pet1.id)
@@ -25,18 +31,24 @@ end
 describe 'As a visitor' do
   describe 'when I visit Pet Show page' do
     it "should see a link to update Pet 'Update Pet'" do
+      shelter1 = Shelter.create(name: 'Dogs and Cats',
+                                address: '1234 spoon.st',
+                                city: 'Tampa',
+                                state: 'Florida',
+                                zip: '34638')
       pet1 = Pet.create(img: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg',
                         name: 'Mittens',
                         description: "He's healthy",
                         approximate_age: '6 years',
                         sex: 'Male',
-                        adoptable: 'Yes')
+                        adoptable: 'Yes',
+                        shelter_id: shelter1.id)
 
       visit "/pets/#{pet1.id}"
-      expect(page).to have_link("Update Pet")
+      expect(page).to have_link('Update Pet')
 
       visit "/pets/#{pet1.id}"
-      click_link "Update Pet"
+      click_link 'Update Pet'
 
       expect(current_path).to eq("/pets/#{pet1.id}/edit")
     end
@@ -46,15 +58,21 @@ end
 describe 'As a visitor' do
   describe 'When I visit a pet show page' do
     it "should see a link to delete a pet 'Delete Pet'" do
+      shelter1 = Shelter.create(name: 'Dogs and Cats',
+                                address: '1234 spoon.st',
+                                city: 'Tampa',
+                                state: 'Florida',
+                                zip: '34638')
       pet1 = Pet.create(img: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg',
                         name: 'Mittens',
                         description: "He's healthy",
                         approximate_age: '6 years',
                         sex: 'Male',
-                        adoptable: 'Yes')
+                        adoptable: 'Yes',
+                        shelter_id: shelter1.id)
 
       visit "/pets/#{pet1.id}"
-      expect(page).to have_button("Delete Pet")
+      expect(page).to have_button('Delete Pet')
     end
   end
 end
