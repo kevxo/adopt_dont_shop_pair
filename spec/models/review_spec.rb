c4238b7c1bb76e649f2600eaf4f13b829478f9c1
Joshua Carey <jdcarey128@gmail.com>
2020-10-13 19:02:42 -0600

    Change 'name' in review table to 'user_name'

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Review, type: :model do
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:user_name) }
     it { should validate_presence_of(:rating) }
     it { should validate_presence_of(:content) }
     it { should validate_presence_of(:title) }
