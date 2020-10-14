0f714cf41b6d968f005addfebe8c54415a8e8861
Kevin Cuadros <kev040798@gmail.com>
2020-10-13 20:37:34 -0400

    Finishes story 4

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -23,6 +23,7 @@ class SheltersController < ApplicationController
 
   def show
     @shelter = Shelter.find(params[:id])
+    @reviews = Review.all
   end
 
   def edit
--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -77,3 +77,49 @@ describe 'As a visitor' do
     end
   end
 end
+
+describe 'As a visitor' do
+  describe 'When I visit a shelters show page' do
+    it 'should show a list of reviews for that shelter' do
+      shelter1 = Shelter.create!(name: 'Dogs and Cats',
+                                 address: '1234 spoon.st',
+                                 city: 'Tampa',
+                                 state: 'Florida',
+                                 zip: '34638')
+      user1 = User.create!(name: 'Bob',
+                           street_address: '1234 Test Dr',
+                           city: 'Denver',
+                           state: 'Colorado',
+                           zip: '12345')
+      user2 = User.create!(name: 'Tony',
+                           street_address: '1234 Review Dr',
+                           city: 'Denver',
+                           state: 'Colorado',
+                           zip: '19345')
+      review1 = Review.create!(title: 'My Rating',
+                               rating: 3,
+                               content: 'review',
+                               shelter_id: shelter1.id,
+                               user_id: user1.id,
+                               name: user1.name)
+      review2 = Review.create!(title: 'My thoughts',
+                               rating: 4,
+                               content: 'Great place for pets',
+                               shelter_id: shelter1.id,
+                               user_id: user2.id,
+                               name: user2.name)
+      visit "/shelters/#{shelter1.id}"
+
+      expect(page).to have_content(review1.title)
+      expect(page).to have_content(review1.rating.to_s)
+      expect(page).to have_content(review1.content)
+      expect(page).to_not have_xpath("//img[contains(@src,'#{review1.picture}')]")
+      expect(page).to have_content(review1.name)
+      expect(page).to have_content(review2.title)
+      expect(page).to have_content(review2.rating.to_s)
+      expect(page).to have_content(review2.content)
+      expect(page).to_not have_xpath("//img[contains(@src,'#{review1.picture}')]")
+      expect(page).to have_content(review2.name)
+    end
+  end
+end
