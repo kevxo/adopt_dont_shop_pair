411de3dd9aaf02e728a34a3d78cf5c051301732f
Joshua Carey <jdcarey128@gmail.com>
2020-10-15 15:13:59 -0600

    Add flash message for no valid user for shelter reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,7 @@ class ReviewsController < ApplicationController
   def create
     user_id = User.find_by(name: params[:review][:user_name])
 
-    # binding.pry
-    if user_id
+    if user_id 
       review = Review.new({
         user_name: params[:review][:user_name],
         title: params[:review][:title],
@@ -17,10 +16,9 @@ class ReviewsController < ApplicationController
         user_id: user_id.id,
         shelter_id: params[:id]
       })
-
       if review.save
         review.save
-        redirect_to "/shelters/#{params[:id]}/reviews/new"
+        redirect_to "/shelters/#{params[:id]}"
       else
         flash[:notice] = 'Review not created: Need title, content, and rating.'
         redirect_to "/shelters/#{params[:id]}/reviews/new"
