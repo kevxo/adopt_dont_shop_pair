1afd6ddc1e87a67314dad9305cae46b124f9ea24
Joshua Carey <jdcarey128@gmail.com>
2020-10-16 16:43:09 -0600

    Add relationships for pet

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -3,5 +3,11 @@ require 'rails_helper'
 describe Pet do
   describe 'relationships' do
     it { should belong_to :shelter }
+    it { should have_many :pet_applications}
+    it { should have_many(:applications).through(:pet_applications)}
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name}
   end
 end
