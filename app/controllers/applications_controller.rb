d792b10c5131b8dc45d9945c1950a5d1192626bd
Joshua Carey <jdcarey128@gmail.com>
2020-10-17 09:37:46 -0600

    Add pet search for application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,6 +2,7 @@ class ApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:application_id])
+    @pet_search_result = Pet.find_by(name: params[:pet_search])
   end
 
 end
