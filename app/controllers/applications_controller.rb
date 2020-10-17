327c1a95ced78c9f8855f264bb232dd5392ab17f
Joshua Carey <jdcarey128@gmail.com>
2020-10-17 12:21:29 -0600

    Revise reviews conditional with != []

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,7 +8,7 @@ class ApplicationsController < ApplicationController
   def update
     application = Application.find(params[:application_id])
     application.update(pet_names: application.pet_names.concat(", #{params[:pet_name]}"))
-    PetApplication.create!(pet_id: params[:pet_id], application_id: params[:application_id])
+    PetApplication.create(pet_id: params[:pet_id], application_id: params[:application_id])
     redirect_to "/applications/#{params[:application_id]}"
   end
 
