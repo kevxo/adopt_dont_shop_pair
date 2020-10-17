8bd3e939562839631d63af3e447a0e0997491b87
Kevin Cuadros <kev040798@gmail.com>
2020-10-17 18:58:18 -0400

    Finish story 17

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -4,39 +4,48 @@ RSpec.describe 'As a visitor' do
   describe 'when I have been take to the new application form' do
     it 'I should see a form to fill out.' do
       user = User.create(name: 'Bob',
-                          street_address: '1234 Test Dr',
-                          city: 'Denver',
-                          state: 'Colorado',
-                          zip: '12345')
+                         street_address: '1234 Test Dr',
+                         city: 'Denver',
+                         state: 'Colorado',
+                         zip: '12345')
       shelter1 = Shelter.create(name: 'Dogs and Cats',
-                                 address: '1234 spoon.st',
-                                 city: 'Tampa',
-                                 state: 'Florida',
-                                 zip: '34638')
+                                address: '1234 spoon.st',
+                                city: 'Tampa',
+                                state: 'Florida',
+                                zip: '34638')
 
       pet1 = Pet.create(img: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg',
-                         name: 'Mittens',
-                         approximate_age: '6 years',
-                         sex: 'Male',
-                         shelter_id: shelter1.id)
+                        name: 'Mittens',
+                        approximate_age: '6 years',
+                        sex: 'Male',
+                        shelter_id: shelter1.id)
 
       pet2 = Pet.create(img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Adorable-animal-cat-20787.jpg',
-                         name: 'Tiger',
-                         approximate_age: '4 years',
-                         sex: 'Male',
-                         shelter_id: shelter1.id)
+                        name: 'Tiger',
+                        approximate_age: '4 years',
+                        sex: 'Male',
+                        shelter_id: shelter1.id)
 
-      user_name =  user.name
+      user_name = user.name
       visit '/applications/new'
 
-      fill_in "user_name", with: user_name
+      fill_in 'user_name', with: user_name
 
       click_button 'Submit'
       application = Application.last
 
-
       expect(current_path).to eq("/applications/#{application.id}")
       expect(page).to have_content('Bob')
+    end
+
+    it 'should return error message when user_name is not part of database' do
+      visit '/applications/new'
+
+      fill_in 'user_name', with: 'bob'
+
+      click_button 'Submit'
+      expect(page).to have_content("Application not created: User couldn't be found.")
+      expect(page).to have_button('Submit')
     end
   end
 end
