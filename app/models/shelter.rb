11383086a1e09146fe28a85393efb4b056bfa632
Kevin Cuadros <kev040798@gmail.com>
2020-10-19 18:47:29 -0400

    Story 32 model methods

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -3,4 +3,22 @@ class Shelter < ApplicationRecord
   has_many :reviews
 
   validates_presence_of :name
+
+  def pet_count
+    self.pets.count
+  end
+
+  def average_rating
+    reviews.pluck(:rating).sum / reviews.pluck(:rating).count
+  end
+
+  def number_of_applications
+    count = 0
+    pets.each do |pet|
+      if  PetApplication.find_by pet_id: pet.id
+        count += 1
+      end
+    end
+    count
+  end
 end
