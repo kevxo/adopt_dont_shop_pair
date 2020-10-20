357cb267c32102d7fe56debc1e485544328578c8
Joshua Carey <jdcarey128@gmail.com>
2020-10-20 16:10:27 -0600

    Add message if pet has no applications

--- spec/features/pets/applications_show_spec.rb
+++ spec/features/pets/applications_show_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe "As a visitor", type: :feature do
                           state: 'CO',
                           zip: '80009')
 
+      user_4 = User.create!(name: "Geraldine Grossman",
+                          street_address: '2112 Olivander Pk',
+                          city: 'Aurora',
+                          state: 'Colorado',
+                          zip: '80018')
+
       shelter_1 = Shelter.create!(name: "Rocky Mountain High", address: "1234 fake st.", city: "Denver", state: "CO", zip: "45505")
 
       pet1 = Pet.create!(img: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg',
@@ -29,13 +35,21 @@ RSpec.describe "As a visitor", type: :feature do
                          sex: 'Male',
                          shelter_id: shelter_1.id)
 
+      pet_2 = shelter_1.pets.create!(img: "https://dogtime.com/assets/uploads/gallery/german-shorthaired-pointer-dogs-and-puppies/german-shorthaired-pointer-dogs-puppies-3.jpg",
+                                   name: "Isabell",
+                                   approximate_age: "5",
+                                   sex: "female",
+                                   description: "Isabell is my favorite and I don't want her to go...but then again, I do!")
+
       application_1 = Application.create!(user_name: user_1.name, user_id: user_1.id, application_status: "Pending")
       application_2 = Application.create!(user_name: user_2.name, user_id: user_1.id, application_status: "Pending")
       application_3 = Application.create!(user_name: user_3.name, user_id: user_1.id, application_status: "Pending")
+      application_4 = Application.create!(user_name: user_4.name, user_id: user_1.id, application_status: "Pending")
 
       pet_app_1 = PetApplication.create!(pet_id: pet1.id, application_id: application_1.id)
       pet_app_2 = PetApplication.create!(pet_id: pet1.id, application_id: application_2.id)
       pet_app_3 = PetApplication.create!(pet_id: pet1.id, application_id: application_3.id)
+      pet_app_4 = PetApplication.create!(pet_id: pet_2.id, application_id: application_4.id)
 
       visit "/pets/#{pet1.id}"
 
@@ -49,6 +63,7 @@ RSpec.describe "As a visitor", type: :feature do
         expect(page).to have_content(application_1.user_name)
         expect(page).to have_content(application_2.user_name)
         expect(page).to have_content(application_3.user_name)
+        expect(page).to_not have_content(application_4.user_name)
       end
 
       within "#applicant-#{application_1.id}" do
@@ -69,6 +84,53 @@ RSpec.describe "As a visitor", type: :feature do
       end
     end
 
-    
+    it "displays a message that if there are no applications for a pet" do
+      user_1 = User.create!(name: 'Bob Woodword',
+                          street_address: '1234 Test Dr',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: '12345')
+
+      user_2  = User.create!(name: 'Jeff Daniels',
+                          street_address: '455 west dr',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: '87709')
+
+      user_3 = User.create!(name: 'Holly Baker',
+                          street_address: '4443 fountain ave',
+                          city: 'Lakewood',
+                          state: 'CO',
+                          zip: '80009')
+
+      shelter_1 = Shelter.create!(name: "Rocky Mountain High", address: "1234 fake st.", city: "Denver", state: "CO", zip: "45505")
+
+      pet1 = Pet.create!(img: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg',
+                         name: 'Mittens',
+                         approximate_age: '6 years',
+                         sex: 'Male',
+                         shelter_id: shelter_1.id)
+
+      pet_2 = shelter_1.pets.create!(img: "https://dogtime.com/assets/uploads/gallery/german-shorthaired-pointer-dogs-and-puppies/german-shorthaired-pointer-dogs-puppies-3.jpg",
+                                  name: "Isabell",
+                                  approximate_age: "5",
+                                  sex: "female",
+                                  description: "Isabell is my favorite and I don't want her to go...but then again, I do!")
+
+      application_1 = Application.create!(user_name: user_1.name, user_id: user_1.id, application_status: "Pending")
+      application_2 = Application.create!(user_name: user_2.name, user_id: user_1.id, application_status: "Pending")
+      application_3 = Application.create!(user_name: user_3.name, user_id: user_1.id, application_status: "Pending")
+
+      pet_app_1 = PetApplication.create!(pet_id: pet1.id, application_id: application_1.id)
+      pet_app_2 = PetApplication.create!(pet_id: pet1.id, application_id: application_2.id)
+      pet_app_3 = PetApplication.create!(pet_id: pet1.id, application_id: application_3.id)
+
+      visit "/pets/#{pet_2.id}/applications"
+
+      within ".pet-applicants" do
+        expect(page).to have_content("No applications for this pet currently.")
+      end
+    end
+
   end
 end
