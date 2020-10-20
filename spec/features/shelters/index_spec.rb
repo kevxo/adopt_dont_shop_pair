97068e38ca7250d71c3a6c9ef0a7adc69d9d7d4d
Kevin Cuadros <kev040798@gmail.com>
2020-10-20 16:59:03 -0400

    Update tests for story 33

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -123,54 +123,42 @@ describe 'As a visitor' do
 
   describe 'If a shelter has approved applications on them' do
     it 'cannot delete shelter' do
-      shelter_1 = Shelter.create!(name: 'Colorado Cares', address: '867 magnolia st',
-                                  city: 'Lakewood', state: 'CO', zip: '80022')
+      user = User.create!(name: 'Bob',
+                          street_address: '1234 Test Dr',
+                          city: 'Denver',
+                          state: 'Colorado',
+                          zip: '12345')
 
-      pet1 = shelter_1.pets.create!(img: 'https://dogtime.com/assets/uploads/gallery/austalian-shepherd-dog-breed-pictures/10-threequarters.jpg',
-                                    name: 'Tony',
-                                    approximate_age: '2',
-                                    sex: 'male',
-                                    description: 'Tony is a wild cracker at times, but is able to calm down and cuddle when needed.')
-      pet2 = shelter_1.pets.create!(img: 'https://dogtime.com/assets/uploads/gallery/german-shorthaired-pointer-dogs-and-puppies/german-shorthaired-pointer-dogs-puppies-3.jpg',
-                                    name: 'Isabell',
-                                    approximate_age: '5',
-                                    sex: 'female',
-                                    description: "Isabell is my favorite and I don't want her to go...but then again, I do!")
-      user_1 = User.create!(name: 'Holly Baker',
-                            street_address: '4443 fountain ave',
-                            city: 'Lakewood',
-                            state: 'CO',
-                            zip: '80009')
-      user_2 = User.create!(name: 'Jeff Daniels',
-                            street_address: '455 west dr',
-                            city: 'Denver',
-                            state: 'Colorado',
-                            zip: '87709')
+      shelter1 = Shelter.create!(name: 'Dogs and Cats',
+                                 address: '1234 spoon.st',
+                                 city: 'Tampa',
+                                 state: 'Florida',
+                                 zip: '34638')
 
-      review_1 = shelter_1.reviews.new(title: 'Colorado Cares is the best', rating: 5,
-                                       content: 'I absolutely love this shelter. I have found the best friend a woman could have!',
-                                       user_name: 'Holly', picture: 'https://tilasto.info/arkkitehti/wp-content/uploads/2019/02/kirkkokivi1.jpg')
-      review_1.user_id = user_1.id
-      review_1.save!
+      pet_1 = Pet.create!(img: 'https://upload.wikimedia.org/wikipedia/commons/a/a3/June_odd-eyed-cat.jpg',
+                          name: 'Mittens',
+                          approximate_age: '6 years',
+                          sex: 'Male',
+                          shelter_id: shelter1.id)
 
-      review_2 = shelter_1.reviews.new(title: 'Ehhhhh', rating: 1,
-                                       content: 'All I can say is nope', user_name: 'Jeff', picture: 'https://cdn.hpm.io/wp-content/uploads/2019/06/25143552/Dogs-1000x750.jpg')
-      review_2.user_id = user_2.id
-      review_2.save!
+      pet_2 = Pet.create!(img: 'https://upload.wikimedia.org/wikipedia/commons/3/38/Adorable-animal-cat-20787.jpg',
+                          name: 'Tiger',
+                          approximate_age: '4 years',
+                          sex: 'Male',
+                          adoptable: 'No',
+                          shelter_id: shelter1.id)
 
-      application_1 = Application.create!(user_name: user_1.name, address: "#{user_1.street_address}, #{user_1.city}, #{user_1.state} #{user_1.zip}",
-                                          description: 'I am an experienced pet owner for 5 years and I just love this pet!',
-                                          pet_names: pet1.name.to_s, user_id: user_1.id)
-      application_2 = Application.create!(user_name: user_2.name, address: "#{user_2.street_address}, #{user_2.city}, #{user_2.state} #{user_2.zip}",
-                                          description: 'I would be a loving owner for any of these pets. Enough said.',
-                                          pet_names: pet2.name.to_s, user_id: user_2.id)
 
-      PetApplication.create!(pet_id: pet1.id, application_id: application_1.id)
-      PetApplication.create!(pet_id: pet2.id, application_id: application_2.id)
+      application_1 = Application.create!(user_name: user.name, user_id: user.id, application_status: 'Pending')
+
+      pet_app_1 = PetApplication.create!(pet_id: pet_1.id, application_id: application_1.id, application_status: 'Approved')
+      pet_app_2 = PetApplication.create!(pet_id: pet_2.id, application_id: application_1.id, application_status: 'Approved')
+
+      application_pets = PetApplication.where(application_id: application_1.id)
 
       visit '/shelters'
 
-      within "#shelter-#{shelter_1.id}" do
+      within "#shelter-#{shelter1.id}" do
         expect(page).to have_button('Delete')
         click_button 'Delete'
       end
