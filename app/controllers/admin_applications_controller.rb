388fffd74cc368cee3c4ef8fb1de8029d8d88956
Joshua Carey <jdcarey128@gmail.com>
2020-10-20 13:10:52 -0600

    Add application_id helper in admin application controller

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -1,13 +1,13 @@
 class AdminApplicationsController < ApplicationController
 
   def show
-    @application = Application.find(params[:application_id])
+    @application = Application.find(application_id)
   end
 
   def update
-    application = Application.find(params[:application_id])
-    application_pets = PetApplication.where(application_id: params[:application_id])
-    pet_application = PetApplication.where(application_id: params[:application_id], pet_id: params[:pet_id])
+    application = Application.find(application_id)
+    application_pets = PetApplication.where(application_id: application_id)
+    pet_application = PetApplication.where(application_id: application_id, pet_id: params[:pet_id])
 
     if params[:commit] == "Approve Pet"
       pet_application.update(application_status: "Approved")
@@ -15,14 +15,19 @@ class AdminApplicationsController < ApplicationController
       pet_application.update(application_status: "Rejected")
     end
 
-    if application_pets.all_approved?(params[:application_id]) && !application_pets.any_pending?(params[:application_id])
+    if application_pets.all_approved?(application_id) && !application_pets.any_pending?(application_id)
       application.update(application_status: "Approved")
       application.adopt_pets
-    elsif !application_pets.all_approved?(params[:application_id]) && !application_pets.any_pending?(params[:application_id])
+    elsif !application_pets.all_approved?(application_id) && !application_pets.any_pending?(application_id)
       application.update(application_status: "Rejected")
     end
 
     redirect_to "/admin/applications/#{params[:application_id]}"
+  end
+
+  private
+  def application_id
+    params[:application_id]
   end
 
 end
